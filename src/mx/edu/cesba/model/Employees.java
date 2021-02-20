24ce6414a3616c99daea1ad05f280ed14709e668
hp <hp@DESKTOP-SL6810D>
2021-02-20 11:29:31 -0600

    Version2

--- src/mx/edu/cesba/model/Employees.java
+++ src/mx/edu/cesba/model/Employees.java
@@ -6,16 +6,82 @@ package mx.edu.cesba.model;
 
 public class Employees {
 
-    public int id;
-    public String firstName;
-    public String lastName;
-    public int age;
-    public String email;
-    public int phoneNumber;
-    public double salary;
-    public String hireDate;
-
-    public void getDetails() {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String email;
+    private int phoneNumber;
+    private double salary;
+    private String hireDate;
+    
+    
+    //metodos para acceder al campo: private int id;
+    public void setid(int id){
+        this.id =id;
+    }
+    public int getid(){
+        return id;
+    }     
+    
+     //metodos para acceder al campo: private String firstName;
+        public void setfirstName(String firstName){
+        this.firstName =firstName;
+    }
+    public String getfirstName(){
+        return firstName;
+    }
+    //metodos para acceder al campo: private String lastName;
+      public void setlastName(String lastName){
+        this.lastName =lastName;
+    }
+    public String getlastName(){
+        return lastName;
+    }
+    //metodos para acceder al campo: private int age;    
+      public void setage(int age){
+            if(age>=18 &&age<65){
+             this.age =age;
+      }else{
+    System.err.println("Edad invalida");
+}
+        
+    }
+    public int getage(){
+      
+        return age;
+    }
+    //metodos para acceder al campo: private String email;  
+      public void setemail(String email){
+        this.email =email;
+    }
+    public String getemail(){
+        return email;
+    }
+    //metodos para acceder al campo: private int phoneNumber;  
+     public void setphoneNumber(int phoneNumber){
+        this.phoneNumber =phoneNumber;
+    }
+    public int getphoneNumber(){
+        return phoneNumber;
+    }
+    //metodos para acceder al campo: private double salary;  
+    public void setsalary(double salary){
+        this.salary =salary;
+    }
+    public double getsalary(){
+        return salary;
+    }
+    //metodos para acceder al campo: private String hireDate;  
+       public void sethireDate(String hireDate){
+        this.hireDate =hireDate;
+    }
+    public String gethireDate(){
+        return hireDate;
+    }
+    
+    
+        public void getDetails() {
         
         System.out.println("id: " + id);
         System.out.println("firstName: " + firstName);
