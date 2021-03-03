57548c7fd08205150a2fac31a7d3e4e2b7ef0811
hp <hp@DESKTOP-SL6810D>
2021-03-02 23:56:44 -0600

    version3

--- src/mx/edu/cesba/Main/HR_Main.java
+++ src/mx/edu/cesba/Main/HR_Main.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Nombre: Luis Javier Navarrete Cruz
+Matricula: 2022235
  */
 package mx.edu.cesba.Main;
 
@@ -11,33 +10,46 @@ import mx.edu.cesba.model.Employees;
 
 public class HR_Main {
     
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Employees e1 = new Employees();
         Employees e2 = new Employees(); 
         
-        e1.id = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su ID"));
-        e1.firstName = JOptionPane.showInputDialog("Ingrese su Nombre");
-        e1.lastName = JOptionPane.showInputDialog("Ingrese el apellido");
-        e1.age = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad"));
-        e1.email = JOptionPane.showInputDialog("Ingrese su email");
-        e1.phoneNumber  = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero telefonico"));
-        e1.salary = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el salario"));
-        e1.hireDate = JOptionPane.showInputDialog("Ingrese la fecha de contratacion");
         
-       /*
-        e2.id = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su ID")); 
-        e2.firstName = JOptionPane.showInputDialog("Ingrese su Nombre");
-        e2.lastName = JOptionPane.showInputDialog("Ingrese el apellido");
-        e2.age = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad"));
-        e2.email = JOptionPane.showInputDialog("Ingrese su email");
-        e2.phoneNumber  = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero telefonico"));
-        e2.salary = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el salario"));
-        e2.hireDate = JOptionPane.showInputDialog("Ingrese la fecha de contratacion");
-        */
-        e1.getDetails();
-        System.out.println("*************");
-        //e2.getDetails();            
-    }
+        e1.setId (Integer.parseInt(JOptionPane.showInputDialog("Ingrese su ID")));
+        e1.setFirstName (JOptionPane.showInputDialog("Ingrese su Nombre"));
+        e1.setLastName(JOptionPane.showInputDialog("Ingrese el apellido"));
+        e1.setAge(Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad")));
+        e1.setEmail(JOptionPane.showInputDialog("Ingrese su email"));
+        e1.setPhoneNumber(Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero telefonico")));       
+        e1.setSalary(Double.parseDouble(JOptionPane.showInputDialog("Ingrese el salario")));
+        e1.setHireDate(JOptionPane.showInputDialog("Ingrese la fecha de contratacion"));
+                 
+                
+                
+    /*            
+      e1.id = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su ID"));
+      e1.firstName = JOptionPane.showInputDialog("Ingrese su Nombre");
+      e1.lastName = JOptionPane.showInputDialog("Ingrese el apellido");
+      e1.age = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad"));
+      e1.email = JOptionPane.showInputDialog("Ingrese su email");
+      e1.phoneNumber  = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero telefonico"));
+      e1.salary = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el salario"));
+      e1.hireDate = JOptionPane.showInputDialog("Ingrese la fecha de contratacion");
+       
+      /*
+//        e2.id = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su ID")); 
+//        e2.firstName = JOptionPane.showInputDialog("Ingrese su Nombre");
+//        e2.lastName = JOptionPane.showInputDialog("Ingrese el apellido");
+//        e2.age = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad"));
+//        e2.email = JOptionPane.showInputDialog("Ingrese su email");
+//        e2.phoneNumber  = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero telefonico"));
+//        e2.salary = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el salario"));
+//        e2.hireDate = JOptionPane.showInputDialog("Ingrese la fecha de contratacion");
+//        */
+       e1.getDetails();
+     System.out.println("*************");
+//        //e2.getDetails();            
+   }
 }
     
 
